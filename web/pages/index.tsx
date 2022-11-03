d4dc6233af26d88eeca759b07dcdcc66aa63384a
Fernanda.dias97@hotmail.com <fernandadias.dev@gmail.com>
2022-11-03 12:42:50 -0300

    build: :tada: first commit

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,12 +1,23 @@
-export default function Home() {
-  fetch('http://localhost:333//pools/count')
-  .then(response => response.json())
-  .then(data => {
-    console.log(data)
-  })
-    
+interface HomeProps {
+  count: number;
+}
 
-    return(
-      <h1>Hello World</h1>
-    )
+export default function Home(props: HomeProps) {
+  return (
+    <h1>Contagem: {props.count}</h1>
+  )
+}
+
+export const getServerSideProps = async () => {
+  const response = await fetch('http://localhost:3333/pools/count')
+  const data = await response.json()
+
+  console.log(data)
+
+  return {
+    pops: {
+      count: data.count,
+    }
+  }
+
 }
